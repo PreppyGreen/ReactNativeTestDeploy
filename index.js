804657c71fcf831810974b243929e110d2a75fce
Oliver Booth <obooth@preppygreen.com>
2020-01-17 14:13:56 +0000

    Add analytics track event

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import Analytics from 'appcenter-analytics';
+
 AppRegistry.registerComponent(appName, () => App);
+
+Analytics.trackEvent("App loaded");
+
+Analytics.trackEvent('Video clicked', { Category: 'Music', FileName: 'favorite.avi' });
