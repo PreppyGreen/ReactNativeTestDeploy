9545738cae8f813c3b5c5e25103bc6d9d76f1db7
Olie Chan <olie.chan@infinityworks.com>
2020-01-23 15:19:20 +0000

    Use data from an api instead of a hard coded array

--- screens/TextAutoCompleteScreen.tsx
+++ screens/TextAutoCompleteScreen.tsx
@@ -1,12 +1,26 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { StyleContext } from '../theme/StyleContext';
 import Autocomplete from 'react-native-autocomplete-input';
+import axios from 'axios';
 
+const api = 'https://jsonplaceholder.typicode.com/todos';
 export default function TextAutoCompleteScreen() {
 	const styleContext = useContext(StyleContext);
 	const [ query, setQuery ] = useState('');
-  const data = filterData(exampleData)(query);
+	const [ exampleData, setExampleData ] = useState([]);
+	const data = filterData(exampleData)(query);
+
+	// Fetch a bunch of data when this component loads for the first time
+	useEffect(() => {
+		async function getAndSetExampleData() {
+			const res = await axios.get(api);
+			const newExampleData = res.data.map((item: any) => item.title);
+			setExampleData(newExampleData);
+		}
+		getAndSetExampleData()
+	}, []);
+
   return (
 		<View style={styleContext.container}>
 			<Autocomplete
@@ -28,13 +42,7 @@ export default function TextAutoCompleteScreen() {
 
 function filterData(data: string[]) {
 	return function filter(query: string) {
-		return data.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+		return data.filter(item => item.toLowerCase()
+			.includes(query.toLowerCase()));
 	}
 }
-
-const exampleData = [
-	'hello',
-	'world',
-	'foo',
-	'bar',
-];
