820cd4ed93bf24c1b3b415bde75b65b71a75b417
Olie Chan <olie.chan@infinityworks.com>
2020-01-26 12:31:43 +0000

    Add mapbox import

--- screens/MapboxScreen.tsx
+++ screens/MapboxScreen.tsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react';
 import { View } from 'react-native';
 import { StyleContext } from '../theme/StyleContext';
 import { Text } from 'react-native-elements';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import { MAPBOX_KEY } from '../config';
+MapboxGL.setAccessToken(MAPBOX_KEY);
+
 
 export default function MapboxScreen() {
 	const styleContext = useContext(StyleContext);
@@ -9,6 +13,8 @@ export default function MapboxScreen() {
 		<View style={styleContext.container}
 		>
 			<Text>ADD MAPS HERE</Text>
+			<MapboxGL.MapView styleURL={MapboxGL.StyleURL.Street}
+			/>
 		</View>
 	);
 }
