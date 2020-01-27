9b67ec6a33b6ceecb0072c378850ba6be9fec658
Olie Chan <olie.chan@infinityworks.com>
2020-01-27 13:27:19 +0000

    Display a modal when tapping one of the icons

--- screens/MapboxScreen.tsx
+++ screens/MapboxScreen.tsx
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
-import { View, Image, Alert } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Modal, StyleSheet } from 'react-native';
 import { StyleContext } from '../theme/StyleContext';
-import { Text } from 'react-native-elements';
+import { Text, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { MAPBOX_KEY } from '../config';
@@ -11,6 +11,7 @@ MapboxGL.setAccessToken(MAPBOX_KEY);
 const UK_COORDINATE = [-3.435973, 55.378052]
 export default function MapboxScreen() {
 	const styleContext = useContext(StyleContext);
+	const [ modalIsOpen, setModalIsOpen ] = useState(false);
 	return (
 		<View style={styleContext.container}
 		>
@@ -25,15 +26,21 @@ export default function MapboxScreen() {
 			>
 				<MapboxGL.Camera centerCoordinate={UK_COORDINATE} zoomLevel={4} />
 				{ pharmacyLocationList
-					.map((coordinate, id) => <Marker coordinate={coordinate} id={`id-${id}`} />) }
+					.map((coordinate, id) => <Marker
+						coordinate={coordinate}
+						id={`id-${id}`}
+						onPress={() => setModalIsOpen(true)}
+					/>) }
 			</MapboxGL.MapView>
+			<Drawer open={modalIsOpen}
+				setOpen={setModalIsOpen}/>
 		</View>
 	);
 }
 
 
 // This should take a coordinate and return a point on the map view
-function Marker({ coordinate, id }: any) {
+function Marker({ coordinate, id, onPress }: any) {
 	return (
 		<MapboxGL.PointAnnotation
 			key={id}
@@ -41,9 +48,43 @@ function Marker({ coordinate, id }: any) {
 			title="Avicenna Pharmacy"
 			coordinate={[ coordinate.longitude, coordinate.lattitude ]}
 		>
-			<Icon name="rocket" size={30} color="#900" onPress={() => {
-				Alert.alert('You just tapped a location');
-			}}/>
+			<Icon name="rocket"
+				size={30}
+				color="#900"
+				onPress={onPress}
+			/>
 		</MapboxGL.PointAnnotation>
 	);
 }
+
+function Drawer({ open, setOpen }: {
+	open: boolean;
+	setOpen: Function;
+}) {
+	return (
+		<View >
+			<Modal animationType="slide"
+				transparent={true}
+				visible={open}
+			>
+				<View style={styles.modalContent}>
+					<Text>Some modal content</Text>
+					<Button type="solid"
+						title="Close the drawer"
+						onPress={() => setOpen(false)}
+					/>
+				</View>
+			</Modal>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	modalContent: {
+		marginTop: 200,
+		backgroundColor: 'white',
+		display: 'flex',
+		flex: 1,
+		justifyContent: 'space-between',
+	}
+});
