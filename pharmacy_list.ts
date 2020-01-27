a4a75eb38c24c3eb09169f0b87f0796efa836653
Olie Chan <olie.chan@infinityworks.com>
2020-01-27 12:42:31 +0000

    Display a few points on the map

---
+++ pharmacy_list.ts
@@ -0,0 +1,23 @@
+type location = {
+	longitude: number;
+	lattitude: number;
+};
+
+export const pharmacyLocationList: location[] = [
+	{
+		longitude: -2.611720,
+		lattitude: 51.420740,
+	},
+	{
+		longitude: -2.187540,
+		lattitude: 52.212710,
+	},
+	{
+		longitude: -0.459150,
+		lattitude: 52.126200,
+	},
+	{
+		longitude: -0.790340,
+		lattitude: 51.196530,
+	},
+];
--- screens/MapboxScreen.tsx
+++ screens/MapboxScreen.tsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
-import { View } from 'react-native';
+import { View, Image, Alert } from 'react-native';
 import { StyleContext } from '../theme/StyleContext';
 import { Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { MAPBOX_KEY } from '../config';
+import { pharmacyLocationList } from '../pharmacy_list';
 MapboxGL.setAccessToken(MAPBOX_KEY);
 
-
+const UK_COORDINATE = [-3.435973, 55.378052]
 export default function MapboxScreen() {
 	const styleContext = useContext(StyleContext);
 	return (
@@ -17,8 +19,31 @@ export default function MapboxScreen() {
 				height: '100%',
 				width: '100%',
 			}}
+				zoomEnabled={true}
+				scrollEnabled={true}
 				styleURL={MapboxGL.StyleURL.Street}
-			/>
+			>
+				<MapboxGL.Camera centerCoordinate={UK_COORDINATE} zoomLevel={4} />
+				{ pharmacyLocationList
+					.map((coordinate, id) => <Marker coordinate={coordinate} id={`id-${id}`} />) }
+			</MapboxGL.MapView>
 		</View>
+	);
+}
+
+
+// This should take a coordinate and return a point on the map view
+function Marker({ coordinate, id }: any) {
+	return (
+		<MapboxGL.PointAnnotation
+			key={id}
+			id={id}
+			title="Avicenna Pharmacy"
+			coordinate={[ coordinate.longitude, coordinate.lattitude ]}
+		>
+			<Icon name="rocket" size={30} color="#900" onPress={() => {
+				Alert.alert('You just tapped a location');
+			}}/>
+		</MapboxGL.PointAnnotation>
 	);
 }
