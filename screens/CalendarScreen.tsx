79fa6cfe242bc4a4e2e23b8f043b11c03bac8e96
Olie Chan <olie.chan@infinityworks.com>
2020-01-27 15:53:40 +0000

    Get the basic calendar in

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
 import { StyleContext } from '../theme/StyleContext';
 import { View, Text } from 'react-native';
-
+import { Calendar } from 'react-native-calendars';
 
 export default function CalendarScreen() {
 	const styleContext = useContext(StyleContext);
 
 	return (
 		<View style={styleContext.container}>
-			<Text>TODO</Text>
+			<Calendar />
 		</View>
 	);
 }
