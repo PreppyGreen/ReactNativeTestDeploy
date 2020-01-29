ae36bbecfc809d6ef02ef22d9f58f4becee1d3ad
Olie Chan <olie.chan@infinityworks.com>
2020-01-29 15:57:04 +0000

    Fix bug where the orders would not refresh

--- proof-of-concept-screens/OrderViewScreen.tsx
+++ proof-of-concept-screens/OrderViewScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 import { StyleContext } from '../theme/StyleContext';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { SUCCESS_GREEN } from '../theme/colors';
@@ -7,33 +8,41 @@ import { percentageWidth, percentageHeight } from '../theme/utils';
 import { NavigationStackProp } from 'react-navigation-stack';
 import Reactotron from 'reactotron-react-native';
 import { OrderType } from '../types/order';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const DOT_SIZE = 40;
 const LINE_WIDTH = 3;
 export default function OrderViewScreen({
-	navigation
+  navigation,
 }: {
-	navigation: NavigationStackProp<{
-		order: OrderType
-	}>
+  navigation: NavigationStackProp<{
+    order: OrderType;
+  }>;
 }) {
-	Reactotron.log('nav props are', navigation.getParam('order'))
+  Reactotron.log('nav props are', navigation.getParam('order'));
   return (
-    <View
-      style={{
-				flex: 1,
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        padding: '10%',
-      }}>
-      <ProgressSection completed={true} text="Sent to pharmacy" />
-      <Line />
-      <ProgressSection completed={false} text="Approved" />
-      <Line />
-      <ProgressSection completed={false} text="Ready for collection" />
-      <Line />
-      <ProgressSection completed={false} text="Collected" />
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          padding: '10%',
+        }}>
+        <ProgressSection completed={true} text="Sent to pharmacy" />
+        <Line />
+        <ProgressSection completed={false} text="Approved" />
+        <Line />
+        <ProgressSection completed={false} text="Ready for collection" />
+        <Line />
+        <ProgressSection completed={false} text="Collected" />
+      </View>
+      <Button
+        type="solid"
+        title="Go back to the landing page"
+        onPress={() => navigation.push('Landing')}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -55,9 +64,10 @@ function ProgressSection({ completed, text }: any) {
 
 function Line() {
   return (
-    <View style={{
-			paddingLeft: DOT_SIZE / 2 - LINE_WIDTH * 1.5,
-		}}>
+    <View
+      style={{
+        paddingLeft: DOT_SIZE / 2 - LINE_WIDTH * 1.5,
+      }}>
       <View
         style={{
           height: percentageHeight(10),
