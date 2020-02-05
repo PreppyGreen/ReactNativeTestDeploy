a8b849a34f5445f59119cb2dbf4d513b0c69c268
Olie Chan <olie.chan@infinityworks.com>
2020-02-05 12:35:55 +0000

    Add a bunch of inputs for the registration screen

--- proof-of-concept-screens/RegistrationScreen.tsx
+++ proof-of-concept-screens/RegistrationScreen.tsx
@@ -1,39 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {	ScrollView, View, Text, StyleSheet, TextInput } from "react-native";
+import { Button } from 'react-native-elements';
 import { percentageWidth, percentageHeight } from '../theme/utils';
+import Reactotron from 'reactotron-react-native';
 
 export default function RegistrationScreen() {
+	const [ firstName, setFirstName ] = useState('');
+	const [ lastName, setLastName ] = useState('');
+	const [ address1, setAddress1 ] = useState('');
+	const [ address2, setAddress2 ] = useState('');
+	const [ address3, setAddress3 ] = useState('');
+	const [ city, setCity ] = useState('');
+	const [ postcode, setPostcode ] = useState('');
+	const [ dob, setDob ] = useState('');
+	const [ phone, setPhone ] = useState('');
+
 	return (
 		<ScrollView contentContainerStyle={styles.container}>
 			<Label label="First name">
-				<TextInput style={styles.textInput} textContentType="name"/>
+				<TextInput style={styles.textInput} textContentType="name" value={firstName} onChangeText={setFirstName}/>
 			</Label>
 			<Label label="Last name" >
-				<TextInput style={styles.textInput} textContentType="name"/>
+				<TextInput style={styles.textInput} textContentType="name" value={lastName} onChangeText={setLastName}/>
 			</Label>
 			<Label label="Address line 1" >
-				<TextInput style={styles.textInput} textContentType="streetAddressLine1"/>
+				<TextInput style={styles.textInput} textContentType="streetAddressLine1" value={address1} onChangeText={setAddress1}/>
 			</Label>
 			<Label label="Address line 2">
-				<TextInput style={styles.textInput} textContentType="streetAddressLine2"/>
+				<TextInput style={styles.textInput} textContentType="streetAddressLine2" value={address2} onChangeText={setAddress2}/>
 			</Label>
 			<Label label="Address line 3">
-				<TextInput style={styles.textInput} />
+				<TextInput style={styles.textInput} value={address3} onChangeText={setAddress3} />
 			</Label>
 			<Label label="City">
-				<TextInput style={styles.textInput} textContentType="addressCity"/>
+				<TextInput style={styles.textInput} textContentType="addressCity" value={city} onChangeText={setCity} />
 			</Label>
 			<Label label="Postcode">
-				<TextInput style={styles.textInput} textContentType="postalCode"/>
+				<TextInput style={styles.textInput} textContentType="postalCode" value={postcode} onChangeText={setPostcode} />
 			</Label>
 			<Label label="Date of birth (DD/MM/YYY)">
-				<TextInput style={styles.textInput} />
+				<TextInput style={styles.textInput} value={dob} onChangeText={setDob} />
 			</Label>
 			<Label label="Phone number">
-				<TextInput style={styles.textInput} textContentType="telephoneNumber"/>
+				<TextInput style={styles.textInput} textContentType="telephoneNumber" value={phone} onChangeText={setPhone}/>
 			</Label>
 
-
+			<Button title="Confirm"
+				type="solid"
+				onPress={() => Reactotron.log('do something')}
+			/>
 		</ScrollView>
 	);
 }
